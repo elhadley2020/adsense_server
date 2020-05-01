602fe1c76ba3f7109168aceb0ea00f8cab003bb1
root <root@alp.my.domain>
2020-05-01 01:06:07 +0000

    added adsense js code

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 func main(){
 	home := func(w http.ResponseWriter, req *http.Request){
 		io.WriteString(w,
-		"<!doctype html><html><head><title>Title</title></head><body><h1>Header 1</h1><p>Paragraph 1</p></body></html>")
+		"<!doctype html><html><head><title>Title</title><script data-ad-client='ca-pub-8840478853854273' async src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js'></script></head><body><h1>Header 1</h1><p>Paragraph 1</p></body></html>")
 		}
 	
 	http.HandleFunc("/", home)
